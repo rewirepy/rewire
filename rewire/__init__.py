66fb36d376453c68ec48b2b38d2f99ae0a3607da
Ivan Vozhakov <gou177@bk.ru>
2024-02-02 02:30:48 +0500

    feat: export everything in __init__

--- rewire/__init__.py
+++ rewire/__init__.py
@@ -1,1 +1,11 @@
-from . import context, dependencies, plugins, config, log, lifecycle
+from . import context, dependencies, plugins, config, log, lifecycle, classproperty, loader, space, store
+from .context import *
+from .dependencies import *
+from .plugins import *
+from .config import *
+from .log import *
+from .lifecycle import *
+from .classproperty import *
+from .loader import *
+from .space import *
+from .store import *
